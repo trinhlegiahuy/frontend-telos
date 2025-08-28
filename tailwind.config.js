140978e8765e5cf1cdfd4f6751db04a8aae8e903
trinhlegiahuy <trinhlegiahuy.biin@gmail.com>
2025-08-28 23:37:04 +0700

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html"
+    "./*/*.html"
   ],
-  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#8C30F5",
+      },
+      fontSize: {
+        H1: ["72px", "98px"],
+        H2: ["48px", "64px"],
+        Lead1: ["18px", "32px"],
+      },
+      spacing: {
+        18: "72px"
+      }
+    },
   },
   plugins: [],
 }
